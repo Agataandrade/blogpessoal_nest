119356406fa18d239a1c0b9217b0ee56bb5f4570
Agataandrade <agatasouandrade@gmail.com>
2025-08-11 18:16:36 -0300

    atulizacoes_02

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { PostagemModule } from './postagem/postagem.module';
 import { Postagem } from './postagem/entities/postagem.entity';
 import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
-import { AuthModule } from './auth/auth.module';
 import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+
 
 @Module({
   imports: [
@@ -26,7 +28,7 @@ import { UsuarioModule } from './usuario/usuario.module';
     AuthModule,
     UsuarioModule,
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
