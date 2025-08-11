1d39cf4a24a218ca56df376a99d7efd5230a376f
Agataandrade <agatasouandrade@gmail.com>
2025-08-11 20:41:50 -0300

    Deploy do Projeto Blog Pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostagemModule } from './postagem/postagem.module';
-import { Postagem } from './postagem/entities/postagem.entity';
-import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
-import { ProdService } from './data/services/prod.service';
 import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+  ConfigModule.forRoot(),
 TypeOrmModule.forRootAsync({
 	useClass: ProdService,
     imports: [ConfigModule],
